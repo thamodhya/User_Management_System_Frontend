04faf2125395b316c0b35c0f3780b80324c1fb6e
unknown <thamodyabandara4@gmail.com>
2024-05-07 13:39:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from './components/common/Navbar';
+import LoginPage from './components/auth/LoginPage';
+import RegistrationPage from './components/auth/RegistrationPage';
+import Footer from './components/common/Footer';
+import UserService from './components/service/UserService';
+import UpdateUser from './components/userspage/UpdateUser';
+import UserManagementPage from './components/userspage/UserManagementPage';
+import ProfilePage from './components/userspage/ProfilePage';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<LoginPage />} />
+            <Route exact path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+
+            {UserService.adminOnly() && (
+              <>
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route path="/admin/user-management" element={<UserManagementPage />} />
+                <Route path="/update-user/:userId" element={<UpdateUser />} />
+              </>
+            )}
+            <Route path="*" element={<Navigate to="/login" />} />‰
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ src/components/userspage/ProfilePage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import UserService from '../service/UserService';
+import { Link } from 'react-router-dom';
+
+function ProfilePage() {
+    const [profileInfo, setProfileInfo] = useState({});
+
+    useEffect(() => {
+        fetchProfileInfo();
+    }, []);
+
+    const fetchProfileInfo = async () => {
+        try {
+
+            const token = localStorage.getItem('token');  
+            const response = await UserService.getYourProfile(token);
+            setProfileInfo(response.ourUsers);
+        } catch (error) {
+            console.error('Error fetching profile information:', error);
+        }
+    };
+
+    return (
+        <div className="profile-page-container">
+            <h2>Profile Information</h2>
+            <p>Name: {profileInfo.name}</p>
+            <p>Email: {profileInfo.email}</p>
+            <p>City: {profileInfo.city}</p>
+            {profileInfo.role === "ADMIN" && (
+                <button><Link to={`/update-user/${profileInfo.id}`}>Update Profile</Link></button>
+            )}
+        </div>
+    );
+}
+
+export default ProfilePage;
